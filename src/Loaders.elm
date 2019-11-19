5c4ec0cd12e3ed37f2886ccac713c9ebbdce7ba8
Jouderian Ferreira Nobre Junior <jouderianjr@gmail.com>
2019-11-19 15:28:13 +0100

    Add comments to code

--- src/Loaders.elm
+++ src/Loaders.elm
@@ -1,17 +1,24 @@
-module Loaders exposing
-    ( audio
-    , ballTriangle
-    , bars
-    , circles
-    , grid
-    , hearts
-    , oval
-    , puff
-    , rings
-    , spinningCircles
-    , tailSpin
-    , threeDots
-    )
+module Loaders exposing (audio, ballTriangle, bars, circles, grid, hearts, oval, puff, rings, spinningCircles, tailSpin, threeDots)
+
+{-| This Package is the Sam Herbert [https://github.com/SamHerbert/SVG-Loaders](svg loaders) implementation for elm
+
+
+## Example
+
+    import Loaders
+
+    view : Html msg
+    view =
+        div [] [ Loaders.TailSpin 40 "#ff0000" ]
+
+PS. all the loaders receives the width as float and the color as string
+
+
+## Loaders
+
+@docs audio, ballTriangle, bars, circles, grid, hearts, oval, puff, rings, spinningCircles, tailSpin, threeDots
+
+-}
 
 import Html exposing (Html)
 import Internal.Audio as Audio
@@ -28,61 +35,73 @@ import Internal.TailSpin as TailSpin
 import Internal.ThreeDots as ThreeDots
 
 
+{-| -}
 audio : Float -> String -> Html msg
 audio =
     Audio.view
 
 
+{-| -}
 bars : Float -> String -> Html msg
 bars =
     Bars.view
 
 
+{-| -}
 oval : Float -> String -> Html msg
 oval =
     Oval.view
 
 
+{-| -}
 ballTriangle : Float -> String -> Html msg
 ballTriangle =
     BallTriangle.view
 
 
+{-| -}
 circles : Float -> String -> Html msg
 circles =
     Circles.view
 
 
+{-| -}
 grid : Float -> String -> Html msg
 grid =
     Grid.view
 
 
+{-| -}
 puff : Float -> String -> Html msg
 puff =
     Puff.view
 
 
+{-| -}
 rings : Float -> String -> Html msg
 rings =
     Rings.view
 
 
+{-| -}
 spinningCircles : Float -> String -> Html msg
 spinningCircles =
     SpinningCircles.view
 
 
+{-| -}
 threeDots : Float -> String -> Html msg
 threeDots =
     ThreeDots.view
 
 
+{-| -}
 tailSpin : Float -> String -> Html msg
 tailSpin =
     TailSpin.view
 
 
+{-| -}
 hearts : Float -> String -> Html msg
 hearts =
     Hearts.view
